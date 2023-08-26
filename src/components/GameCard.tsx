d54a2d2e4bddfc82629c06a7be4c4e1c27e5d2f9
yexuan <yexuan@students.cs.ubc.ca>
2023-08-25 21:26:02 -0700

    replace original images by smaller image

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,8 @@ import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CritiScore from "./criticScore";
+
+import getCroppedImageUrl from "../services/image-url";
 // Game is just an interface and it has nothing to do with data
 interface GameCardProps {
   game: Game;
@@ -9,7 +11,7 @@ interface GameCardProps {
 const GameCard = ({ game }: GameCardProps) => {
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image}></Image>
+      <Image src={getCroppedImageUrl(game.background_image)}></Image>
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent={"space-between"}>
