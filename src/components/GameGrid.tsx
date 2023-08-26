d454cb438203832eac493fa99385ce17b4a668ef
yexuan <yexuan@students.cs.ubc.ca>
2023-08-25 21:52:05 -0700

    show loading skeletons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,9 +1,11 @@
-import { SimpleGrid, Text } from "@chakra-ui/react";
+import { SimpleGrid, Skeleton, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
-
+import GameCardSkeleton from "./GameCardSkeleton";
 const GameGrid = () => {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
   return (
     // display the Game[] to the webpage as a list and deal with the error condition
     <>
@@ -14,10 +16,14 @@ const GameGrid = () => {
         ))}
       </ul> */}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         spacing={10}
         padding="10px"
       >
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardSkeleton key={skeleton}></GameCardSkeleton>
+          ))}
         {games.map((game) => (
           <GameCard key={game.id} game={game}></GameCard> // GameCard supports the format of game
         ))}
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -25,24 +25,26 @@ const useGames = ()=> {
 
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
-
-  
+  const [isLoading, setLoading] = useState(false);
   
   useEffect(() => {
+    setLoading(true);
     const controller = new AbortController();
     apiClient
       .get<FetchGamesResponse>("/games", {signal: controller.signal}) // FetchGamesResponse defined above to limit the res format
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);})
       .catch((err) => { 
         if(err instanceof CanceledError) return; // if(err.name === 'CanceledError') return;
         setError(err.message);
+        setLoading(false);
       });
      
       return ()=> controller.abort();
   }, []); // []: If you do not want the useEffect callback to run on every render 
-
-  return {games, error};
-  
+ 
+  return {games, error, isLoading};
 }
 export default useGames
 
