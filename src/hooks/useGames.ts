0fb467df20c563618b8a93ba05d904eb92a2e32e
yexuan <jyx88056601@gmail.com>
2023-09-26 22:04:38 -0700

    create a reuseble APIClient

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,19 +2,33 @@ import useData from "./useData"; // previous hook to get the data
 import {FetchResponse, Game, GameQuery} from "../hooks/interfaces" 
 import { useQuery } from  "@tanstack/react-query"
 import apiClient from "../services/api-client";
+import APIClient from "../services/apiClient";
+
+const api = new APIClient<Game>("/games");
+
 const useGames = (gameQuery: GameQuery) => {
   // anytime the variables below changes, refetching the data from server
-  const params = {
-    genres: gameQuery.genre?.id,
-    parent_platforms: gameQuery.platform?.id,
-    ordering: gameQuery.sortOrder,
-    search: gameQuery.searchText
-  };
+ 
 
   return useQuery<FetchResponse<Game>,Error>({
     queryKey: ["games", gameQuery],
-    queryFn: () => apiClient.get<FetchResponse<Game>>("/games", {params: params})
-    .then(res => res.data)
+    // params : {
+    //   genres: gameQuery.genre?.id,
+    //   parent_platforms: gameQuery.platform?.id,
+    //   ordering: gameQuery.sortOrder,
+    //   search: gameQuery.searchText
+    // }
+    // queryFn: () => apiClient.get<FetchResponse<Game>>("/games", {params: params})
+    // .then(res => res.data)
+    queryFn:  () => api.getData({ 
+      params : {
+        genres: gameQuery.genre?.id,
+        parent_platforms: gameQuery.platform?.id,
+        ordering: gameQuery.sortOrder,
+        search: gameQuery.searchText
+      }
+    }),
+    staleTime: 24 * 60 * 60 * 1000,
   });
 };
 
