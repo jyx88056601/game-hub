74d7fa4af13cb992e3fd36b8d9e97ca95c962caf
yexuan <jyx88056601@gmail.com>
2023-10-01 17:51:12 -0700

    refactor API service and resolve bugs

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,19 +1,15 @@
-import {Genre, FetchResponse} from "../hooks/interfaces"
-import { useQuery } from "@tanstack/react-query" 
+import { Genre, FetchResponse } from "../hooks/interfaces"
+import { useQuery } from "@tanstack/react-query"
 import genres from "../data/genres";
 import APIClient from "../services/apiClient";
-// const useGenres = ()=> useData<Genre>("./genres");
-
-
 const api = new APIClient<Genre>("/genres");
 
-const useGenres =  () => useQuery<FetchResponse<Genre>,Error>({
+const useGenres = () => useQuery<FetchResponse<Genre>, Error>({
     queryKey: ["Genres"],
     // queryFn: () => apiClient.get<FetchResponse<Genre>>("/genres").then(res => res.data),
-    queryFn: () => api.getData({}),
+    queryFn: () => api.getData(),
     staleTime: 24 * 60 * 60 * 1000,
-    initialData: {count : genres.length, results: genres}
+    initialData: { count: genres.length, results: genres }
 })
 export default useGenres;
 
-  
