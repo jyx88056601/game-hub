768be33c1798e1c8bb87bc2a3633cf5a6db961e8
Ethan Jiang <jyx88056601@gmail.com>
2023-11-10 19:20:33 -0800

    fix bugs

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client"; 
-import "./index.css";
-import theme from "./theme";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { RouterProvider } from "react-router-dom"; 
-import router from "./routing/Routes";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import theme from './theme';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { RouterProvider } from 'react-router-dom';
+import router from './routing/Routes';
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <ColorModeScript
-        initialColorMode={theme.config.initialColorMode}
-      ></ColorModeScript>
+      <ColorModeScript initialColorMode="light"></ColorModeScript>
       <QueryClientProvider client={queryClient}>
-         <RouterProvider router={router}/>
+        <RouterProvider router={router} />
         <ReactQueryDevtools />
       </QueryClientProvider>
     </ChakraProvider>
